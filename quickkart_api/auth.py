4178d0a336f80535e6949e82dd742747a702da31
envaleed <mark_2k9@live.com>
2021-11-04 16:59:07 -0500

    added auth error

--- quickkart_api/auth.py
+++ quickkart_api/auth.py
@@ -1,11 +1,13 @@
 from quickkart_api import app
 from quickkart_api.models import Users
 from flask_jwt import JWT, jwt_required, current_identity
+from flask import abort
 
 def authenticate(username, password):
     user = Users.query.filter_by(username=username).first()
     if user and user.check_password(password):
         return user
+    return abort(500, "Authentication failed")
 
 def identity(payload):
     return Users.query.filter(Users.id == payload['identity']).scalar()
