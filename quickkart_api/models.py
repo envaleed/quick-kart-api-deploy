46fdae8b469a241b2c10a23c1242a5c2c577914a
envaleed <mark_2k9@live.com>
2021-11-04 18:01:13 -0500

    change data type of bcrypt hash string

--- quickkart_api/models.py
+++ quickkart_api/models.py
@@ -27,14 +27,14 @@ class Users(db.Model):
     id = db.Column(db.Integer,primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=True)
     email = db.Column(db.String(64), index=True, unique=True, nullable=True)
-    password_hash = db.Column(db.LargeBinary, nullable=True)
+    password_hash = db.Column(db.String(128), nullable=True)
     orders = db.relationship('Orders', backref="users", lazy=True)
 
     def __repr__(self):
         return '<Users %r>' % self.id
 
     def set_password(self,password):
-        self.password_hash = bcrypt.generate_password_hash(password)
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
