889983859097f6635d339691c786ac469743d40b
envaleed <mark_2k9@live.com>
2021-11-04 16:21:41 -0500

    uri fixed again

--- quickkart_api/__init__.py
+++ quickkart_api/__init__.py
@@ -4,8 +4,11 @@ from flask import Flask
 #app = Flask(__name__, instance_relative_config=True)
 app = Flask(__name__)
 #app.config.from_pyfile('config.py')
+uri = os.environ.get('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SECRET KET']=os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI']= uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 from quickkart_api import models, serializers, views, auth, errors
