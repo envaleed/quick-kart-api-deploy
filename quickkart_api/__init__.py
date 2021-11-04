d32025edbd7359c362e79b666bf11119bc5f16a3
envaleed <mark_2k9@live.com>
2021-11-04 16:06:57 -0500

    added track modifications rule

--- quickkart_api/__init__.py
+++ quickkart_api/__init__.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 #app.config.from_pyfile('config.py')
 app.config['SECRET KET']=os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 from quickkart_api import models, serializers, views, auth, errors
