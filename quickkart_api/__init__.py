4dde13cefed325c0cda70883ab751a9c7baf6f24
envaleed <mark_2k9@live.com>
2021-11-04 16:15:38 -0500

    uri edited

--- quickkart_api/__init__.py
+++ quickkart_api/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 #app.config.from_pyfile('config.py')
 app.config['SECRET KET']=os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 from quickkart_api import models, serializers, views, auth, errors
