ccd775e38854243258c9113c8e09b3e299e04cae
envaleed <mark_2k9@live.com>
2021-11-04 19:08:19 -0500

    Secret Key added

--- quickkart_api/__init__.py
+++ quickkart_api/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 uri = os.environ.get('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SECRET KET']=os.environ.get('SECRET_KEY')
+app.config['SECRET_KET']=os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI']= uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
